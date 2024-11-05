1c0f27b3017fc3552bd5f34281f6f0cb08b099b5
Sameeowrai <sameeowrai@gmail.com>
2024-11-06 02:17:35 +0330

    Create start menu

--- main.cpp
+++ main.cpp
@@ -4,4 +4,32 @@
 using namespace std;
 
 int main(){
+    Menu<int> menu("Menu",{
+        {"Start",defaultColor,0},
+        {"High Score",defaultColor,1},
+        {"Settings",defaultColor,2},
+        {"Exit", "\e[0;31m",3}
+    },0.01,10);
+    while(1){
+        int op = menu.Get();
+        switch (op)
+        {
+        case 0:{
+            break;
+        }
+
+        case 1:{
+            break;
+        }
+
+        case 2:{
+            break;
+        }
+        case 3:{
+            exit(0);
+        }
+        default:
+            break;
+        }
+    }   
 }
